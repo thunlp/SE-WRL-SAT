dc61215c2ae697c015327fbea819fa6b04599ba8
qifanchao <>
2019-01-16 17:36:47 +0800

    Update some parameters.

--- run_SAT.sh
+++ run_SAT.sh
@@ -1,4 +1,4 @@
 make SAT_new
-time ./SAT_new -train data/train_sample.txt -read-sememe data/SememeFile.txt -read-hownet data/Word_Sense_Sememe_File.txt -save-vocab vocab.txt -output-word word-vec.txt -output-sense sense-vec.txt -output-sememe sememe-vec.txt -size 200 -alpha 0.025 -min-count 50 -window 5 -sample 1e-3 -negative 10 -threads 20 -iter 5
+time ./SAT_new -train data/train_sample.txt -read-sememe data/SememeFile.txt -read-hownet data/Word_Sense_Sememe_File.txt -save-vocab vocab.txt -output-word word-vec.txt -output-sense sense-vec.txt -output-sememe sememe-vec.txt -size 200 -alpha 0.025 -min-count 50 -window 8 -sample 1e-4 -negative 25 -threads 20 -iter 3
 python EvalWordSim.py word-vec.txt
 python EvalAnalogy.py word-vec.txt
